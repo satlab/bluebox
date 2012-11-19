15e6948c3e9677c81edfb5f5d7314192f9c26942
Jeppe Ledet-Pedersen <jlp@satlab.org>
2012-11-19 20:37:03 +0100

    firmware: set preamble in firmware
    Signed-off-by: Jeppe Ledet-Pedersen <jlp@satlab.org>

--- software/firmware/spi.c
+++ software/firmware/spi.c
@@ -35,88 +35,7 @@ uint8_t front = 0;
 uint8_t back  = 1;
 
 static volatile unsigned char spi_mode = SPI_MODE_IDLE;
-
-void spi_rx_start(void)
-{
-	spi_mode = SPI_MODE_RX;
-	swd_disable();
-
-	led_on(LED_RECEIVE);
-
-	data[front].progress = 0;
-	data[front].size = DATA_LENGTH;
-
-	spi_enable();
-	spi_enable_it();
-}
-
-void spi_rx_done(void)
-{
-	spi_disable_it();
-	spi_disable();
-	swd_enable();
-
-	led_off(LED_RECEIVE);
-	spi_mode = SPI_MODE_IDLE;
-}
-
-void spi_tx_start(void)
-{
-	spi_mode = SPI_MODE_TX;
-	swd_disable();
-
-	led_on(LED_TRANSMIT);
-
-	data[front].progress = 0;
-	data[front].training = conf.training_bytes;
-	
-	spi_enable();
-	spi_enable_it();
-}
-
-void spi_tx_done(void)
-{
-	spi_disable_it();
-	spi_disable();
-	swd_enable();
-
-	led_off(LED_TRANSMIT);
-	spi_mode = SPI_MODE_IDLE;
-}
-
-bool spi_tx_allowed(void)
-{
-	bool allow = false;
-
-	swd_disable();
-
-	if (spi_mode == SPI_MODE_RX)
-		goto out;
-
-	spi_mode = SPI_MODE_TX;
-	allow = true;
-
-out:
-	return allow;
-}
-
-void spi_rx_task(void)
-{
-	if (data[back].flags & FLAG_RX_READY) {
-		Endpoint_SelectEndpoint(IN_EPADDR);
-		Endpoint_AbortPendingIN();
-		Endpoint_Write_Stream_LE(&data[back], sizeof(data[back]), NULL);
-		Endpoint_ClearIN();
-		data[back].flags &= ~FLAG_RX_READY;
-	}
-}
-
-void data_done(void)
-{
-	data[front].flags |= FLAG_RX_READY;
-	back = front;
-	front = !front;
-}
+static char preamble[CALLSIGN_LENGTH + FSM_LENGTH];
 
 static inline uint8_t __attribute__ ((pure)) popcount(uint8_t num)
 {
@@ -165,9 +84,105 @@ static inline __attribute__ ((pure)) int rx_frame_spi_length(uint8_t type)
 	if (conf.do_viterbi)
 		bytes = (bytes + VITERBI_TAIL) * VITERBI_RATE;
 
-	bytes += CUB_LENGTH + FSM_LENGTH;
-
 	return bytes;
+}
+
+static inline __attribute__ ((pure)) int tx_frame_fsm(unsigned int data)
+{
+	int short_frame_limit = SHORT_FRAME_LIMIT + CSP_OVERHEAD;
+
+	if (conf.do_rs)
+		short_frame_limit += RS_LENGTH;
+
+	if (conf.do_viterbi)
+		short_frame_limit = (short_frame_limit + VITERBI_TAIL) * VITERBI_RATE;
+
+	return (data <= short_frame_limit) ? SHORT_FRAME_MARKER : LONG_FRAME_MARKER;
+}
+
+void spi_rx_start(void)
+{
+	spi_mode = SPI_MODE_RX;
+	swd_disable();
+
+	led_on(LED_RECEIVE);
+
+	data[front].progress = 0;
+	data[front].size = DATA_LENGTH;
+
+	spi_enable();
+	spi_enable_it();
+}
+
+void spi_rx_done(void)
+{
+	spi_disable_it();
+	spi_disable();
+	swd_enable();
+
+	led_off(LED_RECEIVE);
+	spi_mode = SPI_MODE_IDLE;
+}
+
+void spi_tx_start(void)
+{
+	spi_mode = SPI_MODE_TX;
+	swd_disable();
+
+	strncpy(preamble, conf.callsign, CALLSIGN_LENGTH);
+	preamble[CALLSIGN_LENGTH] = tx_frame_fsm(data[front].size);
+
+	led_on(LED_TRANSMIT);
+
+	data[front].progress = 0;
+	data[front].training = conf.training_bytes;
+	
+	spi_enable();
+	spi_enable_it();
+}
+
+void spi_tx_done(void)
+{
+	spi_disable_it();
+	spi_disable();
+	swd_enable();
+
+	led_off(LED_TRANSMIT);
+	spi_mode = SPI_MODE_IDLE;
+}
+
+bool spi_tx_allowed(void)
+{
+	bool allow = false;
+
+	swd_disable();
+
+	if (spi_mode == SPI_MODE_RX)
+		goto out;
+
+	spi_mode = SPI_MODE_TX;
+	allow = true;
+
+out:
+	return allow;
+}
+
+void spi_rx_task(void)
+{
+	if (data[back].flags & FLAG_RX_READY) {
+		Endpoint_SelectEndpoint(IN_EPADDR);
+		Endpoint_AbortPendingIN();
+		Endpoint_Write_Stream_LE(&data[back], sizeof(data[back]), NULL);
+		Endpoint_ClearIN();
+		data[back].flags &= ~FLAG_RX_READY;
+	}
+}
+
+void flip_buffers(void)
+{
+	data[front].flags |= FLAG_RX_READY;
+	back = front;
+	front = !front;
 }
 
 ISR(INT6_vect)
@@ -177,41 +192,48 @@ ISR(INT6_vect)
 
 ISR(SPI_STC_vect)
 {
-	static uint8_t errs, type;
+	static uint8_t errs, type, byte;
 
 	if (spi_mode == SPI_MODE_TX) {
 		if (data[front].training > 0) {
 			spi_write_data(conf.training_symbol);
 			data[front].training--;
 		} else {
-			spi_write_data(data[front].data[data[front].progress++]);
+			if (data[front].progress < (CALLSIGN_LENGTH + FSM_LENGTH))
+				byte = preamble[data[front].progress];
+			else
+				byte = data[front].data[data[front].progress - (CALLSIGN_LENGTH + FSM_LENGTH)];
+			data[front].progress++;
+			spi_write_data(byte);
 		}
 
-		if (data[front].progress > data[front].size) {
+		if (data[front].progress > (data[front].size + CALLSIGN_LENGTH + FSM_LENGTH)) {
 			spi_tx_done();
 			adf_set_rx_mode();
 		}
 	} else {
 		data[front].data[data[front].progress++] = spi_read_data();
 
-		if (data[front].progress == FSM_POSITION) {
-			errs = frame_cuberrs(data[front].data);
-			if (errs > SYNC_WORD_TOLERANCE * 2) {
-				spi_rx_done();
-				adf_set_threshold_free();
-				return;
+		if (data[front].size == DATA_LENGTH) {
+			if (data[front].progress == FSM_POSITION) {
+				errs = frame_cuberrs(data[front].data);
+				if (errs > SYNC_WORD_TOLERANCE * 2) {
+					spi_rx_done();
+					adf_set_threshold_free();
+					return;
+				}
 			}
-		}
 
-		/* Set frame length from FSM */
-		if (data[front].progress == FSM_POSITION + 1) {
-			type = frame_type(data[front].data[FSM_POSITION]);
-			data[front].size = rx_frame_spi_length(type);
+			if (data[front].progress == FSM_POSITION + 1) {
+				type = frame_type(data[front].data[FSM_POSITION]);
+				data[front].size = rx_frame_spi_length(type);
+				data[front].progress = 0;
+			}
 		}
 
 		if (data[front].progress >= data[front].size) {
 			spi_rx_done();
-			data_done();
+			flip_buffers();
 			adf_set_threshold_free();
 			return;
 		}

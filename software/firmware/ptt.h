ea9c1cc1c922a371efcb00f53be1ad91a11dcd7f
Hans Peter Mortensen <hanspetermortensen@gmail.com>
2013-01-24 22:21:18 +0100

    bluebox: changed push-to-talk to enable extern LNA control.

--- software/firmware/ptt.h
+++ software/firmware/ptt.h
@@ -8,6 +8,7 @@
 #define DIR_RF_CONTROL		DDRD
 #define PIN_RF_TX		2
 #define PIN_RF_RX		3
+#define PIN_EXT_LNA     5
 
 /* PA_BIAS/EXT_LNA pins */
 #define PORT_PALNA_CONTROL	PORTB
@@ -19,6 +20,7 @@ static inline void ptt_high(void)
 {
 	PORT_RF_CONTROL &= ~_BV(PIN_RF_RX);
 	PORT_RF_CONTROL |=  _BV(PIN_RF_TX);
+	PORT_RF_CONTROL &= ~_BV(PIN_EXT_LNA);
 
 	PORT_PALNA_CONTROL |= _BV(PIN_PA_BIAS_EN);
 	PORT_PALNA_CONTROL |= _BV(PIN_EXT_PTT);
@@ -31,11 +33,12 @@ static inline void ptt_low(void)
 
 	PORT_RF_CONTROL &= ~_BV(PIN_RF_TX);
 	PORT_RF_CONTROL |=  _BV(PIN_RF_RX);
+	PORT_RF_CONTROL |=  _BV(PIN_EXT_LNA);
 }
 
 static inline void ptt_init(void)
 {
-	DIR_RF_CONTROL    |= _BV(PIN_RF_TX) | _BV(PIN_RF_RX);
+	DIR_RF_CONTROL    |= _BV(PIN_RF_TX) | _BV(PIN_RF_RX) | _BV(PIN_EXT_LNA);
 	DIR_PALNA_CONTROL |= _BV(PIN_PA_BIAS_EN) | _BV(PIN_EXT_PTT);
 	ptt_low();
 }
